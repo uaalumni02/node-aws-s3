949fdc051330427d90b3ad44d364340764f76cb2
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-06-10 19:06:18 -0400

    add random filenames

--- src/api/profile.js
+++ src/api/profile.js
@@ -3,6 +3,7 @@ import aws from "aws-sdk";
 import multer from "multer";
 import path from "path";
 import fs from "fs";
+import crypto from "crypto";
 var upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
@@ -13,16 +14,21 @@ const s3 = new aws.S3({
 });
 
 router.post("/", upload.single("file"), function (req, res, next) {
+  console.log(req.file)
   if (req.file) {
-    uploadFile(req.file.path);
+    uploadFile(req.file.path); //add argument for filename
   }
 });
+
+const randomString = (size = 5) => {
+  return crypto.randomBytes(size).toString("base64").slice(0, size);
+};
 
 const uploadFile = (fileName) => {
   const fileContent = fs.readFileSync(fileName);
   const params = {
-    Bucket: "uaalumni-practice",
-    Key: "upload.jpg",
+    Bucket: process.env.BUCKETNAME,
+    Key: randomString(), // insert random key name and not hardcode; use crypto to generate random strings; append to original filename; its in req.file
     Body: fileContent,
   };
   s3.upload(params, function (err, data) {
