fb2af1e347f94bccd035c10ae85bfa71e58b4778
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-06-11 18:07:44 -0400

    get signed url

--- src/api/profile.js
+++ src/api/profile.js
@@ -8,34 +8,49 @@ var upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 
-const s3 = new aws.S3({
+const S3 = new aws.S3({
   accessKeyId: process.env.ACCESSKEYID,
   secretAccessKey: process.env.SECRETACCESSKEY,
+  signatureVersion: "v4",
 });
 
 router.post("/", upload.single("file"), function (req, res, next) {
   if (req.file) {
-    uploadFile(req.file.path, req.file.originalname);
+    uploadFile(req.file.path, req.file.originalname, (uploadJSONResponse) => {
+      return res.status(201).json(uploadJSONResponse);
+    });
   }
 });
 
 const randomString = (size = 5) => {
-  return crypto.randomBytes(size).toString("base64").slice(0, size);
+  return crypto.randomBytes(size).toString("hex");
 };
 
-const uploadFile = (fileName, originalName) => {
+const uploadFile = (fileName, originalName, cb) => {
   const fileContent = fs.readFileSync(fileName);
   const params = {
     Bucket: process.env.BUCKETNAME,
-    Key: randomString().concat('_', originalName),
+    Key: randomString().concat("_", originalName),
     Body: fileContent,
   };
-  s3.upload(params, function (err, data) {
+  S3.upload(params, async (err, data) => {
+    console.log(data);
     if (err) {
       throw err;
     }
     console.log(`File uploaded successfully. ${data.Location}`);
+    const presignedUrl = await S3.getSignedUrlPromise("getObject", {
+      Bucket: process.env.BUCKETNAME,
+      Key: data.key,
+      Expires: 60 * 60,
+    });
+    cb({
+      localFilePath: fileName, 
+      remoteFilePath: presignedUrl, 
+    });
   });
 };
 
+
+// Display image in browser instead of downloading
 export default router;
