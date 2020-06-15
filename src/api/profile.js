2bbc7001aef3d7d3c41801777b68b192c521aae5
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-06-15 19:04:39 -0400

    match local & aws console fileNames

--- src/api/profile.js
+++ src/api/profile.js
@@ -12,43 +12,43 @@ const S3 = new aws.S3({
   secretAccessKey: process.env.SECRETACCESSKEY,
   signatureVersion: "v4",
 });
+
+let randomValue = "";
+
+const randomString = (size = 5) => {
+  randomValue = crypto.randomBytes(size).toString("hex");
+  return randomValue;
+};
+
 const storage = multer.diskStorage({
   destination: function (request, file, callback) {
-      callback(null, './uploads/');
+    callback(null, "./uploads/");
   },
   filename: function (request, file, callback) {
-    console.log(file)
-      // generate the filename, + the crypto stuff  
-      callback(null, file.originalname + '_' + Date.now())
-  }
+    callback(null, randomString().concat("_", file.originalname));
+  },
 });
 
 const upload = multer({ storage: storage });
 
-let contentData = ""
+let contentData = "";
 
 router.post("/", upload.single("file"), function (req, res, next) {
   if (req.file) {
-    contentData = req.file.mimetype
+    contentData = req.file.mimetype;
     uploadFile(req.file.path, req.file.originalname, (uploadJSONResponse) => {
       return res.status(201).json(uploadJSONResponse);
     });
   }
 });
 
-const randomString = (size = 5) => {
-  return crypto.randomBytes(size).toString("hex");
-};
-
 const uploadFile = (fileName, originalName, cb) => {
   const fileContent = fs.readFileSync(fileName);
   const params = {
     Bucket: process.env.BUCKETNAME,
-    Key: randomString().concat("_", originalName),
+    Key: randomValue.concat("_", originalName),
     Body: fileContent,
     ContentType: contentData,
-    //random string is being appended to extension 
-    //file name local does not match what is in console
   };
   S3.upload(params, async (err, data) => {
     if (err) {
@@ -68,11 +68,15 @@ const uploadFile = (fileName, originalName, cb) => {
 };
 router.post("/multiple", upload.array("files", 12), function (req, res, next) {
   if (req.files) {
-    for(var i = 0; i < req.files.length; i++) {
-    uploadFile(req.files[i].path, req.files[i].originalname, (uploadJSONResponse) => {
-      return res.status(201).json(uploadJSONResponse);
-    });
-  }
+    for (var i = 0; i < req.files.length; i++) {
+      uploadFile(
+        req.files[i].path,
+        req.files[i].originalname,
+        (uploadJSONResponse) => {
+          return res.status(201).json(uploadJSONResponse);
+        }
+      );
+    }
   }
 });
 
